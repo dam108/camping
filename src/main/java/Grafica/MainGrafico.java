b28abadf22bde3c8851d64ad333dafa5e6429e88
Doval <Doval@DESKTOP-TVDMRBS>
2020-04-18 02:58:55 +0200

    desde casa 18/04/2020 - Version 1.0 - Se refactoriza el main y se añade control de validación del dni, se corrige un problema con el boton de editar parametros

--- src/main/java/Grafica/MainGrafico.java
+++ src/main/java/Grafica/MainGrafico.java
@@ -3,15 +3,11 @@ package Grafica;
 import Ficheros.*;
 import static Ficheros.Ficheros.rutaProperties;
 import Logica.*;
-import java.awt.Color;
-import java.awt.Desktop;
-import java.awt.Font;
-import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
 import javax.swing.*;
 
 public class MainGrafico extends javax.swing.JFrame {
-
     JButton[] tablero;
     public Param p = new Param();
     int numBoton;
@@ -26,6 +22,7 @@ public class MainGrafico extends javax.swing.JFrame {
 
         setLocationRelativeTo(null);  //ventana centrada en pantalla
         tablero = new JButton[Camping.parcelas.size()]; //definimos los 16 botones
+        
         for (int i = 0; i < Camping.parcelas.size(); i++) {
             tablero[i] = new JButton();
             tablero[i].addActionListener(new java.awt.event.ActionListener() {
@@ -35,28 +32,36 @@ public class MainGrafico extends javax.swing.JFrame {
                 }
 
             });
+            
             tablero[i].setName("jButton" + Integer.toString(i));
-            tablero[i].setFont(new java.awt.Font("Tahoma", 0, 25).deriveFont(Font.BOLD));
+            tablero[i].setFont(new java.awt.Font("Tahoma", 0, 25)
+                    .deriveFont(Font.BOLD));
             nomBoton = tablero[i].getName();
             numBoton = Integer.parseInt(nomBoton.substring(7,
                     nomBoton.length()));
+            
             if (Camping.parcelas.get(i).ocupado) {
                 tablero[i].setBackground(Color.red);
             } else {
-                tablero[i].setBackground(Color.DARK_GRAY/*new Color(54, 187, 253)*/);
+                tablero[i].setBackground(Color.DARK_GRAY);
             }
 
             tablero[i].setVerticalTextPosition(SwingConstants.CENTER);
             tablero[i].setHorizontalTextPosition(SwingConstants.CENTER);
             tablero[i].setForeground(Color.black);
             tablero[i].setText(Integer.toString(i + 1));
+            
             if (numBoton >= 0 && numBoton < 10) {
-                tablero[i].setIcon(new javax.swing.ImageIcon("img\\Tienda.PNG"));
+                tablero[i].setIcon(new javax.swing.ImageIcon(
+                        "img\\Tienda.PNG"));
             } else if (numBoton >= 10 && numBoton < 30) {
-                tablero[i].setIcon(new javax.swing.ImageIcon("img\\Caravana.PNG"));
+                tablero[i].setIcon(new javax.swing.ImageIcon(
+                        "img\\Caravana.PNG"));
             } else {
-                tablero[i].setIcon(new javax.swing.ImageIcon("img\\Bungalow.PNG"));
+                tablero[i].setIcon(new javax.swing.ImageIcon(
+                        "img\\Bungalow.PNG"));
             }
+            
             Botonera.add(tablero[i]);
         }
     }
@@ -64,87 +69,29 @@ public class MainGrafico extends javax.swing.JFrame {
     private void FActionPerformed(java.awt.event.ActionEvent evt) {
         nomBoton = ((JButton) evt.getSource()).getName();
         numBoton = Integer.parseInt(nomBoton.substring(7, nomBoton.length()));
-        boolean electricidad;
+        String dni="";
+        Tienda(dni);
+        Caravana(dni);
+        Bungalow();
+    }
+
+    public void Bungalow() {
+        String dni;
         String ad;
         double precio;
-        String dni;
-        //Tienda
-        if (Camping.parcelas.get(numBoton) instanceof Tienda) {
-            if (!Camping.parcelas.get(numBoton).ocupado) {
-                try {
-                    do {
-                        dni = JOptionPane.showInputDialog(this, "Introduce el DNI"
-                                + " del huesped");
-                    } while (dni.equals(""));
-                    int opcion = JOptionPane.showOptionDialog(
-                            null, "Quieres electricidad?", "Selector de opciones",
-                            //ventana, mensaje, título
-                            JOptionPane.YES_NO_CANCEL_OPTION,
-                            JOptionPane.QUESTION_MESSAGE,
-                            null, // icono: null para icono por defecto
-                            new Object[]{"Si", "No"}, // opciones
-                            //null para YES, NO y CANCEL
-                            "Si" //opción con el foco
-                    );
-                    if (opcion == 0) {
-                        electricidad = true;
-                    } else {
-                        electricidad = false;
-                    }
-                    tablero[numBoton].setBackground(Color.red);
-                    ((Tienda) Camping.parcelas.get(numBoton)).checkIn(dni,
-                            electricidad);
-
-                } catch (NullPointerException ex) {
-                    System.err.printf("%nError: %s", ex.getMessage());
-                }
-
-            } else {
-                precio = Camping.parcelas.get(numBoton).checkOut(p);
-                JOptionPane.showMessageDialog(this, "El importe es de " + precio);
-                tablero[numBoton].setBackground(Color.DARK_GRAY/*new Color(54, 187, 253)*/);
-            }
-        }
-        // Caravana
-        if (Camping.parcelas.get(numBoton) instanceof Caravana) {
-            //no se permite checkout de menos de 10 dias
-            if (!Camping.parcelas.get(numBoton).ocupado) {
-                try {
-                    do {
-                        dni = JOptionPane.showInputDialog(this, "Introduce el DNI"
-                                + " del huesped");
-                    } while (dni.equals(""));
-                    tablero[numBoton].setBackground(Color.red);
-                    ((Caravana) Camping.parcelas.get(numBoton)).checkIn(dni);
-                } catch (NullPointerException ex) {
-                    System.err.printf("%nError: %s", ex.getMessage());
-                }
-            } else {
-                precio = ((Caravana) Camping.parcelas.get(numBoton)).checkOut(p);
-                if (precio == -1) {
-                    JOptionPane.showMessageDialog(this,
-                            "No se puede hacer checkOut antes de 10 dias de"
-                            + " estancia");
-                } else {
-                    tablero[numBoton].setBackground(Color.DARK_GRAY/*new Color(54, 187, 253)*/);
-                    JOptionPane.showMessageDialog(this, "El importe es de "
-                            + precio);
-                }
-            }
-        }
         // bungalow
         if (Camping.parcelas.get(numBoton) instanceof Bungalow) {
 
             if (!Camping.parcelas.get(numBoton).ocupado) {
                 try {
                     do {
-                        dni = JOptionPane.showInputDialog(this, "Introduce el DNI"
-                                + " del huesped");
-                    } while (dni.equals(""));
+                        dni = JOptionPane.showInputDialog(this,
+                                "Introduce el DNI del huesped");
+                    } while (!comprobarDNI(dni));
                     try {
                         do {
-                            ad = JOptionPane.showInputDialog(this, "Introduce el numero de"
-                                    + " adultos");
+                            ad = JOptionPane.showInputDialog(this,
+                                    "Introduce el numero de adultos");
                         } while (Integer.valueOf(ad) == 0);
 
                         tablero[numBoton].setBackground(Color.red);
@@ -161,12 +108,91 @@ public class MainGrafico extends javax.swing.JFrame {
             } else {
                 System.out.println(
                         ((Bungalow) Camping.parcelas.get(numBoton)).adultos);
-                precio = ((Bungalow) Camping.parcelas.get(numBoton)).checkOut(p);
+                precio = ((Bungalow) Camping.parcelas.get(numBoton))
+                        .checkOut(p);
                 System.out.println(precio);
-                JOptionPane.showMessageDialog(this, "El importe es de " + precio);
-                tablero[numBoton].setBackground(Color.DARK_GRAY/*new Color(54, 187, 253)*/);
+                JOptionPane.showMessageDialog(this, "El importe es de " 
+                        + precio);
+                tablero[numBoton].setBackground(Color.DARK_GRAY);
             }
         }
+    }
+
+    public String Caravana(String dni) {
+        double precio;
+        // Caravana
+        if (Camping.parcelas.get(numBoton) instanceof Caravana) {
+            //no se permite checkout de menos de 10 dias
+            if (!Camping.parcelas.get(numBoton).ocupado) {
+                try {
+                    do {
+                        dni = JOptionPane.showInputDialog(this,
+                                "Introduce el DNI del huesped");
+                    } while (!comprobarDNI(dni));
+                    tablero[numBoton].setBackground(Color.red);
+                    ((Caravana) Camping.parcelas.get(numBoton)).checkIn(dni);
+                } catch (NullPointerException ex) {
+                    System.err.printf("%nError: %s", ex.getMessage());
+                }
+            } else {
+                precio = ((Caravana) Camping.parcelas.get(numBoton))
+                        .checkOut(p);
+                if (precio == -1) {
+                    JOptionPane.showMessageDialog(this,
+                    "No se puede hacer checkOut antes de"+
+                    p.getDiasMinimosCaravana()+" dias de estancia");
+                } else {
+                    tablero[numBoton].setBackground(Color.DARK_GRAY);
+                    JOptionPane.showMessageDialog(this, "El importe es de "
+                            + precio);
+                }
+            }
+        }
+        return dni;
+    }
+
+    public String Tienda(String dni) {
+        boolean electricidad;
+        double precio;
+        //Tienda
+        if (Camping.parcelas.get(numBoton) instanceof Tienda) {
+            if (!Camping.parcelas.get(numBoton).ocupado) {
+                try {
+                    do {
+                        dni = JOptionPane.showInputDialog(this,
+                                "Introduce el DNI del huesped");
+                    } while (!comprobarDNI(dni));
+                    int opcion = JOptionPane.showOptionDialog(
+                            null,"Quieres electricidad?","Selector de opciones",
+                            //ventana, mensaje, título
+                            JOptionPane.YES_NO_CANCEL_OPTION,
+                            JOptionPane.QUESTION_MESSAGE,
+                            null, // icono: null para icono por defecto
+                            new Object[]{"Si", "No"}, // opciones
+                            //null para YES, NO y CANCEL
+                            "Si" //opción con el foco
+                    );
+                    if (opcion == 0) {
+                        electricidad = true;
+                    } else {
+                        electricidad = false;
+                    }
+                    tablero[numBoton].setBackground(Color.red);
+                    ((Tienda) Camping.parcelas.get(numBoton)).checkIn(dni,
+                            electricidad);
+                    
+                } catch (NullPointerException ex) {
+                    System.err.printf("%nError: %s", ex.getMessage());
+                }
+                
+            } else {
+                precio = Camping.parcelas.get(numBoton).checkOut(p);
+                JOptionPane.showMessageDialog(this, "El importe es de "
+                        + precio);
+                tablero[numBoton].setBackground(Color.DARK_GRAY);
+            }
+        }
+        return dni;
     }
 
     /**
@@ -265,9 +291,18 @@ public class MainGrafico extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void JB_Edit_ParamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_Edit_ParamActionPerformed
-        JOptionPane.showMessageDialog(this, "Tienes que modificar el archivo"
-                + " a continuacion y despues de modificarlo darle al boton de actualizar.");
-        abrirFichero(rutaProperties);
+        try{
+            int abrirF = JOptionPane.showConfirmDialog(this, "¿Quieres"
+                    + " modificar el fichero de configuración?."
+                    + " Si lo haces tienes que darle al boton de actualizar"
+                    + " despues de modificar y guardar el archivo");
+            if (abrirF == 0){
+                abrirFichero(rutaProperties);
+            }
+        }
+        catch(NullPointerException ex){
+            System.err.printf("%nError: %s", ex.getMessage());
+        }
     }//GEN-LAST:event_JB_Edit_ParamActionPerformed
 
     private void JB_Update_ParamActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_Update_ParamActionPerformed
@@ -278,7 +313,8 @@ public class MainGrafico extends javax.swing.JFrame {
 
     private void JB_AyudaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_AyudaActionPerformed
         JOptionPane.showMessageDialog(this, "Funcionamiento:\n"
-                + "Este programa permite la gestión de las entradas y salidas del camping.\n"
+                + "Este programa permite la gestión de las entradas y salidas"
+                + " del camping.\n"
                 + "Cuando deseamos hacer un Check In en alguna de las parcelas"
                 + " simplemente presionamos el boton que corresponde\na esa"
                 + " parcela y introduciomos los datos que se nos solicitan.\n"
@@ -287,8 +323,12 @@ public class MainGrafico extends javax.swing.JFrame {
                 + "El color gris se corresponde con una parcela vacia.\n"
                 + "El color rojo se corresponde con una parcela ocupada.\n"
                 + "Por ultimo tenemos varios botones en la parte inferior:\n"
-                + "Editar Parametros: Se nos abre un archivo con la configuracion para que podemos modificarla en caso de necesitarlo.\n"
-                + "Actualizar Parametros: Sirve para que el programa recargue la configuracion del archivo anterior justo despues de la modificacion.\n"
+                + "Editar Parametros: Se nos abre un archivo con la "
+                + "configuracion para que podemos modificarla en caso"
+                + " de necesitarlo.\n"
+                + "Actualizar Parametros: Sirve para que el programa recargue"
+                + " la configuracion del archivo anterior justo despues de la"
+                + " modificacion.\n"
                 + "Ayuda: Esto mismo que estas consultando.\n"
                 + "About: Información sobre el autor del programa.\n"
                 + "Salir: Cierra el programa.");
@@ -296,11 +336,11 @@ public class MainGrafico extends javax.swing.JFrame {
 
     private void JB_AboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_AboutActionPerformed
         JOptionPane.showMessageDialog(this, "Datos del Programa:\n"
-                + "Autor: Jose Angel Doval Fraga\n"
-                + "Email de contacto: dovaldam108@gmail.com\n"
-                + "Fecha de creación: 17/04/2020\n"
-                + "Versión: 1.0\n"
-                + "Repositorio en GitHub: https://github.com/dam108/camping.git");
+            + "Autor: Jose Angel Doval Fraga\n"
+            + "Email de contacto: dovaldam108@gmail.com\n"
+            + "Fecha de creación: 17/04/2020\n"
+            + "Versión: 1.0\n"
+            + "Repositorio en GitHub: https://github.com/dam108/camping.git");
     }//GEN-LAST:event_JB_AboutActionPerformed
 
     private void JB_SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_SalirActionPerformed
@@ -317,6 +357,21 @@ public class MainGrafico extends javax.swing.JFrame {
                     "Error",
                     JOptionPane.ERROR_MESSAGE);
         }
+    }
+    
+    public static boolean comprobarDNI(String dni){
+        if(dni.length() == 9){
+            String numeros = dni.substring(0, 8);
+            for (int i = 0; i < numeros.length(); i++) {
+                char a = numeros.charAt(i);
+                if (!Character.isDigit(a)) return false;
+            }
+            String letra = dni.substring(8);
+            if (!Character.isLetter(letra.charAt(0))) return false;
+            
+            return true;
+        }
+        else return false;
     }
 
     /**
